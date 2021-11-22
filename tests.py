970283260310ea6504965d304939ab99cfc1b7ce
nixs-dev <joabewick@gmail.com>
2021-11-22 17:07:47 -0300

    Windows exe available

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from Controllers.Downloader import Downloader
 
-d = Downloader('https://www.youtube.com/watch?v=u-VMMQA-d00', '')
-print(d.get_video_info().thumbnail_url)
+d = Downloader('https://www.youtube.com/watch?v=CBXLc2qAvrU', '')
+d.get_video_info()
+
+print(d.video.streams)
